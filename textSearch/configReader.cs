ed57fcf99d0b3f5f62d200e806dff2c4a1b3065b
saadati944 <saadati944@gmail.com>
2020-03-31 13:42:20 +0430

    Adding configReader contents.

--- textSearch/configReader.cs
+++ textSearch/configReader.cs
@@ -6,8 +6,62 @@ using System.Threading.Tasks;
 
 namespace textSearch
 {
-    class configReader
+    class configReader : IDisposable
     {
+        public string _path;
+        public Dictionary<string, string> configs = new Dictionary<string, string>();
 
+        public configReader(string filepath)
+        {
+            _path = filepath;
+        }
+        public void readFile()
+        {
+            bool comment = false;
+            foreach (string x in System.IO.File.ReadLines(_path))
+            {
+                if (x == "")
+                    continue;
+                if (x.StartsWith("//"))
+                    continue;
+                if (x.StartsWith("/*"))
+                    comment = !comment;
+                if (x.EndsWith("*/"))
+                {
+                    comment = !comment;
+                    continue;
+                }
+                if (comment)
+                    continue;
+                if (!x.Contains(':'))
+                    continue;
+                setVal(x);
+            }
+        }
+        void setVal(string kandv)
+        {
+            string key = kandv.Substring(0, kandv.IndexOf(':'));
+            string value = kandv.Substring(key.Length, kandv.Length - key.Length);
+
+            key = cutSpaces(key);
+            value = cutSpaces(value);
+
+            if (key == "" || value == "")
+                return;
+
+            configs.Add(key, value);
+        }
+        string cutSpaces(string v)
+        {
+            while (v.StartsWith("	") || v.StartsWith(" "))
+                v = v.Substring(1);
+            while (v.EndsWith("	") || v.EndsWith(" "))
+                v = v.Substring(0, v.Length - 1);
+            return v;
+        }
+        public void Dispose()
+        {
+
+        }
     }
 }
