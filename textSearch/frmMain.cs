8a92a4a93c50339eff9724188a56069100df3e1b
saadati944 <saadati944@gmail.com>
2020-03-31 14:33:58 +0430

    Read config file to set path and format.

--- textSearch/frmMain.cs
+++ textSearch/frmMain.cs
@@ -15,7 +15,23 @@ namespace textSearch
         public frmMain()
         {
             InitializeComponent();
+            configReader r = new configReader(Application.StartupPath+ "\\textsearch.config");
+            r.readFile();
+            try
+            {
+                path = r.configs["path"].Split('?');
+            }
+            catch { }
+
+            try
+            {
+                format = r.configs["format"].Split(',');
+            }
+            catch { }
+            r.Dispose();
         }
+        string[] path;
+        string[] format;
 
         private void btnFind_Click(object sender, EventArgs e)
         {
