d0026941e38041c3a91715a9d8ea2f3f20700b16
saadati944 <saadati944@gmail.com>
2020-04-05 21:48:42 +0430

    Fill frmAdd with its controls.

--- textSearch/frmAdd.Designer.cs
+++ textSearch/frmAdd.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtValue.Location = new System.Drawing.Point(0, 0);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(562, 20);
+            this.txtValue.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdd.Location = new System.Drawing.Point(0, 20);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(562, 25);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
             // frmAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(562, 45);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtValue);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmAdd";
             this.Text = "frmAdd";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
